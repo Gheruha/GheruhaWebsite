1de50d7d951ba11aa03cd3bd9015be3127607bdd
Gheruha <gheruham@icloud.com>
2024-01-11 20:48:54 +0200

    Config for working

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+	import '../app.css';
+	import './styles.css';
+</script>
+
+<div class="app">
+		<slot />
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+    export let data;
+</script>
+  
+
+  <div class="flex flex-col items-center pt-12">
+    <h1 class="text-red-500 font-bold text-5xl pb-12">To do 📝</h1>
+
+    <form method="POST" class = "pb-6">
+        <label class = "text-xl">add a to do:
+            <input type="text" class = "bg-zinc-900 rounded-xl border" name = "description">
+        </label>
+    </form>
+
+    {#each data.todos as todo}
+    <div class="w-2/5 border border-zinc-900 rounded-2xl hover:border-red-500 bg-zinc-900 h-16 flex items-center justify-center">
+      <h1 class="text-2xl">{todo.description}</h1>
+    </div>
+    <br>
+    {/each}
+  </div>
+  
+  <style>
+    /* Add any additional styles here */
+  </style>
+  
